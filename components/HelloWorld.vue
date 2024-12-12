5003bdeb30e1101b81f304bfee9f22772fc0d50f
Mohsen Pakfetrat <mohsenpk1370@gmail.com>
2024-12-12 13:57:22 +0330

    initialize repo

---
+++ components/HelloWorld.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const { myLayer } = useAppConfig()
+</script>
+
+<template>
+  <div class="h-screen bg-red-400">
+    <h1>Hello World!</h1>
+    <pre>{{ myLayer }}</pre>
+  </div>
+</template>
