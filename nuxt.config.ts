c7d4f7bbacd810a0e224d19e5486f9338d0384b9
Mohsen Pakfetrat <mohsenpk1370@gmail.com>
2024-12-12 14:28:10 +0330

    fix the paths

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const currentDir = dirname(fileURLToPath(import.meta.url));
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss"],
+  tailwindcss: {
+    cssPath: join(currentDir, './assets/css/tailwind.css'),
+    configPath: join(currentDir, './tailwind.config.js'),
+  }
 })
