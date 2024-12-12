ac2fa9fdff5d2b1f4712cbfc9fedee2c9c893b80
Mohsen Pakfetrat <mohsenpk1370@gmail.com>
2024-12-12 14:19:13 +0330

    customize tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      color: {
+        brand: '#ff00f8',
+      },
+    },
+  },
+  plugins: [],
+}
+
